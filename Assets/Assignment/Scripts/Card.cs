0bd836ae4e3c2c549681df0d695e0de4650f33ba
LungMilk <joeplace913@gmail.com>
2024-03-29 16:27:38 -0400

    Started to work on the selecting animation coroutine

--- Assets/Assignment/Scripts/Card.cs
+++ Assets/Assignment/Scripts/Card.cs
@@ -12,20 +12,21 @@ public class Card : MonoBehaviour
     public TextMeshProUGUI DamageText;
     public TextMeshProUGUI DescriptionText;
     SpriteRenderer spr;
-    Rigidbody2D rgd2d;
+    public Rigidbody2D rgd2d;
 
     int cost;
     string cardType;
     int health;
     int damage;
 
-
+    int offset;
 
     // Start is called before the first frame update
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
         rgd2d = GetComponent<Rigidbody2D>();
+        randomization();
     }
 
     // Update is called once per frame
@@ -37,5 +38,29 @@ public class Card : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log(this.ToString() + " Touched");
+
+        StartCoroutine(selectAnimation());
+    }
+
+    public void randomization()
+    {
+        costText.text = cost.ToString();
+        CardTypeText.text = cardType;
+        HealthText.text = health.ToString();
+        DamageText.text = damage.ToString();
+    } 
+
+    IEnumerator selectAnimation()
+    {
+        while (offset < 1)
+        {
+            rgd2d.transform.Translate(transform.up * 2 * Time.deltaTime);
+            yield return null;
+        }
+        while (offset > -8)
+        {
+            rgd2d.transform.Translate(transform.up * 2 * Time.deltaTime);
+            yield return null;
+        } 
     }
 }
