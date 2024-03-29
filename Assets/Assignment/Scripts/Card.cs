2f1056d820e7104ba83378e15f31f80c6a025772
LungMilk <joeplace913@gmail.com>
2024-03-29 16:09:46 -0400

    Scene set up and card class has initial parent values

--- Assets/Assignment/Scripts/Card.cs
+++ Assets/Assignment/Scripts/Card.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Card : MonoBehaviour
 {
+    public TextMeshProUGUI costText;
+    public TextMeshProUGUI CardTypeText;
+    public TextMeshProUGUI HealthText;
+    public TextMeshProUGUI DamageText;
+    public TextMeshProUGUI DescriptionText;
+    SpriteRenderer spr;
+    Rigidbody2D rgd2d;
+
+    int cost;
+    string cardType;
+    int health;
+    int damage;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spr = GetComponent<SpriteRenderer>();
+        rgd2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
